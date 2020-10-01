ae11a239da83aa19a438619edebf193bc26bb736
Giovanni Agosta <agosta@acm.org>
2020-10-02 01:48:51 +0200

    [Fixes to sw.js]

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-var cacheName = 'lexarcana-pwa';
-var appShellFiles = [
+const staticCacheName = 'lexarcana-pwa';
+var filesToCache = [
   'style.css',
   'icon/la.png',
   'https://pyodide-cdn2.iodide.io/v0.15.0/full/pyodide.js',
@@ -12,9 +12,9 @@ var appShellFiles = [
 self.addEventListener('install', (e) => {
   console.log('[Service Worker] Install');
   e.waitUntil(
-    caches.open(cacheName).then((cache) => {
+    caches.open(staticCacheName).then((cache) => {
           console.log('[Service Worker] Caching all');
-      return cache.addAll(contentToCache);
+      return cache.addAll(filesToCache);
     })
   );
 });
@@ -24,7 +24,7 @@ self.addEventListener('fetch', (e) => {
     caches.match(e.request).then((r) => {
           console.log('[Service Worker] Fetching resource: '+e.request.url);
       return r || fetch(e.request).then((response) => {
-                return caches.open(cacheName).then((cache) => {
+                return caches.open(staticCacheName).then((cache) => {
           console.log('[Service Worker] Caching new resource: '+e.request.url);
           cache.put(e.request, response.clone());
           return response;
