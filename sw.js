9a14eaa108e3fb5b137ea81a9e83c2cfd4caa620
Giovanni Agosta <agosta@acm.org>
2020-10-02 02:33:54 +0200

    [Fixed sw.js]

--- sw.js
+++ sw.js
@@ -1,41 +1,29 @@
-const staticCacheName = 'lexarcana-pwa';
-var filesToCache = [
+let cacheName = 'lexarcana-pwa';
+let filesToCache = [
+  '/',
+  '/style.css',
   '/lexarcana.html',
-]
-
-var filesNotToCache = [
-  'style.css',
-  'icon/la.png',
-  'pyodide.js',
-  'handlebars.js',
+  '/icon/la.png',
+  '/pyodide.js',
+  '/handlebars.js',
   'fonts.googleapis.com/css2?family=Marcellus+SC&display=swap',
   'fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic|Material+Icons',
-];
+]
 
-
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
+self.addEventListener("install", function(e) {
   e.waitUntil(
-    caches.open(staticCacheName).then((cache) => {
-          console.log('[Service Worker] Caching all');
-          return cache.addAll(filesToCache);
-    }).catch(function (err) {
-          console.log('[Service Worker] Installation failed: ', err);
-    });
-  );
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((r) => {
-          console.log('[Service Worker] Fetching resource: '+e.request.url);
-          return r || fetch(e.request).then((response) => {
-                return caches.open(staticCacheName).then((cache) => {
-          console.log('[Service Worker] Caching new resource: '+e.request.url);
-          cache.put(e.request, response.clone());
-          return response;
-        });
-      });
+    caches.open(cacheName).then(function(cache) {
+      console.log("adding files to cache");
+      return cache.addAll(filesToCache)
     })
-  );
-});
+  )
+})
+
+self.addEventListener("fetch", function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      console.log("fetching from cache");
+      return response || fetch(e.request)
+    })
+  )
+})
