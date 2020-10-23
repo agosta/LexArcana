ce847ddbfee94a19541d9fcf24f3f6b1de575186
Giovanni Agosta <agosta@acm.org>
2020-10-23 19:49:50 +0200

    [Feature] Allow print background

--- sw.js
+++ sw.js
@@ -24,7 +24,7 @@ self.addEventListener("fetch", function(e) {
   e.respondWith(
     caches.match(e.request).then(function(response) {
       console.log("fetching from cache");
-      return response || fetch(e.request)
+      return fetch(e.request) || response;
     })
   )
 })
